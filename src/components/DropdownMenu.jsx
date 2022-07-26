a254bf9ad08729d13d7c76ea112a26fcf53f25bb
Gaël <gael77@hotmail.fr>
2022-07-26 11:07:40 +0200

    custom dropdown

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -6,19 +6,37 @@ const DropdownMenu = ({ options, selected, setInput }) => {
   const customStyles = {
     option: (provided, state) => ({
     ...provided,
-    backgroundColor: state.isSelected && 'green',
-    color: state.isSelected ? 'white' : 'green',
+    "&:hover" : {
+      backgroundColor: !state.isSelected && 'rgba(90, 111, 7, .2)',
+    },
+    backgroundColor: state.isSelected && 'rgb(90, 111, 7)',
+    color: state.isSelected ? 'white' : 'rgb(90, 111, 7)',
     cursor: 'pointer'
   }),
-    control: (provided, state) => ({
-    ...provided,
-    border: state.isSelected && '1px solid green',
-    boxShadow: 'none'
-  }),
+    control: (provided, state) => {
+      console.log(state);
+      return ({
+        ...provided,
+        "&:hover" : {
+          outline: state.isFocused ? '2px solid rgb(147, 173, 24)' : "1px solid rgb(90,111,7)",
+        },
+        border: 0,
+        outline: state.isFocused ? '2px solid rgb(147, 173, 24)' : '1px solid rgb(90, 111, 7)',
+        boxShadow: 'none',
+        minHeight: '33px'
+      })
+    },
+    valueContainer: (provided, state) => ({
+      ...provided,
+      fontSize: "13.33px"
+    }),
+    dropdownIndicator: (provided, state) => ({
+      ...provided,
+      padding: "4px 8px"
+    }),
   }
   
   function handleChange(e) {
-    console.log(e.value);
     setInput(e.value)
   }
 

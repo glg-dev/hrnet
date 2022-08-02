ab4ecdd4a7417d2fafc283d07a5f91ac5c939ee4
Gaël <gael77@hotmail.fr>
2022-08-02 15:58:46 +0200

    tests refacto

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./layouts/Header";
 import CreateEmployee from "./pages/CreateEmployee";
 import EmployeesList from "./pages/EmployeesList";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<CreateEmployee />} />
           <Route path='/employees' element={<EmployeesList />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>

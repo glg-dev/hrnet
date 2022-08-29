b68d23fffd9a7d3131cfcacd6a5fa3cd8731f795
Gaël <gael77@hotmail.fr>
2022-08-29 21:51:01 +0200

    table pagination

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
+import { ColumnDef, PaginationState, getPaginationRowModel, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeEmployee } from '../feature/employeeSlice';
@@ -57,6 +57,7 @@ const Table = () => {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   })
 
   function handleDelete(id) {
@@ -107,6 +108,66 @@ const Table = () => {
               ))}
             </tbody>
           </table>
+          <div className="pagination">
+            <button 
+              className="to-first"
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {'<<'}
+            </button>
+            <button 
+              className="to-prev"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {'<'}
+            </button>
+            <div className="get-page">
+              <div>Page</div>
+              <div>
+                {table.getState().pagination.pageIndex + 1} of{' '}
+                {table.getPageCount()}
+              </div>
+            </div>
+            <button 
+              className="to-next"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              {'>'}
+            </button>
+            <button 
+              className="to-last"
+              onClick={() => table.setPageIndex(table.getPageCount() -1)}
+              disabled={!table.getCanNextPage()}
+            >
+              {'>>'}
+            </button>
+            <div className="set-page">
+              Go to page : 
+              <input 
+                type="number"
+                defaultValue={table.getState().pagination.pageIndex + 1}
+                onChange={e => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0
+                  table.setPageIndex(page)
+                }}
+              />
+            </div>
+            <select
+              value={table.getState().pagination.pageSize}
+              onChange={e => {
+                table.setPageSize(Number(e.target.value))
+              }}
+            >
+              {[10, 20, 30, 40, 50].map(pageSize => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       )
     

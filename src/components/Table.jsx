90170003cd09edc18323f19010e92f5b8edde563
Gaël <gael77@hotmail.fr>
2022-09-01 11:39:16 +0200

    responsive

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -134,41 +134,43 @@ const Table = () => {
             </tbody>
           </table>
           <div className="pagination">
-            <button 
-              className="to-first"
-              onClick={() => table.setPageIndex(0)}
-              disabled={!table.getCanPreviousPage()}
-            >
-              {'<<'}
-            </button>
-            <button 
-              className="to-prev"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              {'<'}
-            </button>
             <div className="get-page">
-              <div>Page</div>
-              <div>
-                {table.getState().pagination.pageIndex + 1} of{' '}
-                {table.getPageCount()}
+              <button 
+                className="to-first"
+                onClick={() => table.setPageIndex(0)}
+                disabled={!table.getCanPreviousPage()}
+              >
+                {'<<'}
+              </button>
+              <button 
+                className="to-prev"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                {'<'}
+              </button>
+              <div className="get-page__text">
+                <div>Page</div>
+                <div>
+                  {table.getState().pagination.pageIndex + 1} of{' '}
+                  {table.getPageCount()}
+                </div>
               </div>
+              <button 
+                className="to-next"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                {'>'}
+              </button>
+              <button 
+                className="to-last"
+                onClick={() => table.setPageIndex(table.getPageCount() -1)}
+                disabled={!table.getCanNextPage()}
+              >
+                {'>>'}
+              </button>
             </div>
-            <button 
-              className="to-next"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              {'>'}
-            </button>
-            <button 
-              className="to-last"
-              onClick={() => table.setPageIndex(table.getPageCount() -1)}
-              disabled={!table.getCanNextPage()}
-            >
-              {'>>'}
-            </button>
             <div className="set-page">
               Go to page : 
               <input 
